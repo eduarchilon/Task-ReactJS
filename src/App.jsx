d974a0d275e8bc8e7c1ddc1bd919caebb62db1fe
Javier Valdez <jv.javierval@gmail.com>
2022-05-31 17:02:38 -0300

    feat: add pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/home/Home'
+import AddTask from './pages/add-task/AddTask'
+import EditTask from './pages/edit-task/EditTask'
 
-function App() {
+export function App() {
   return (
-    <div>App</div>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/addtask" element={<AddTask />} />
+          <Route path="/edittask" element={<EditTask />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
