906c367c6279401d0255331b228fd40f240f8ecb
Eduar <83920925+eduarchilon@users.noreply.github.com>
2022-06-01 15:19:41 -0300

    feat: git commit solution

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,28 +1,13 @@
 import React from 'react'
-<<<<<<< HEAD
-=======
-import Task from '../../components/task/Task'
->>>>>>> 883a4a7f4812d4489cc43935fd02299c5aa25c93
 import Labels from '../../components/labels/Labels'
 import TaskSection from '../../components/taskSection/TaskSection'
 import '../home/home.css'
 
 function Home() {
   return (
-<<<<<<< HEAD
-    <div className='home'>
-      <Labels />
-      <TaskSection />
-=======
-    <div className="container">
-      <h1>All Tasks</h1>
-      <input className="search-input" placeholder="Search"></input>
-
-      <main className="home__main-container">
+    <div className='container home'>
         <Labels />
-        <Task />
-      </main>
->>>>>>> 883a4a7f4812d4489cc43935fd02299c5aa25c93
+        <TaskSection />
     </div>
   )
 }
