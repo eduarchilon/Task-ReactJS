7f2636a173ee7be86748aa5f8899fb3199630f5c
Eduar <83920925+eduarchilon@users.noreply.github.com>
2022-06-01 16:02:33 -0300

    feat: home styles

--- src/components/taskSection/TaskSection.jsx
+++ src/components/taskSection/TaskSection.jsx
@@ -4,8 +4,8 @@ import '../../style.css'
 
 export default function TaskSection() {
   return (
-    <div className="task__container">
-      <h2 className='title'>All Task</h2>
+    <div className="task__section">
+      <h1 className='title'>All Task</h1>
       <input type="search" placeholder='Buscar' className='input-search'/>
       <TaskContainer />
       <button type='button' className='btn btn-plus'>+</button>
