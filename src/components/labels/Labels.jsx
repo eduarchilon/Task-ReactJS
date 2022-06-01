643e925a787e3a77ba314f0f595e674e086908cd
Javier Valdez <jv.javierval@gmail.com>
2022-06-01 17:30:15 -0300

    feat: fix bug on Labels.jsx

--- src/components/labels/Labels.jsx
+++ src/components/labels/Labels.jsx
@@ -1,14 +1,30 @@
 import React from 'react'
-import './labels.css'
+import './Labels.css'
 
 function Labels() {
   return (
     <div className="label__container">
-      <ul className='label__list'>
-        <li className='label-item'><a className='label-link' href='#'>All</a></li>
-        <li className='label-item'><a className='label-link' href="#">Groceries</a></li>
-        <li className='label-item'><a className='label-link' href="#">College</a></li>
-        <li className='label-item'><a className='label-link' href="#">Payments</a></li>
+      <ul className="label__list">
+        <li className="label-item">
+          <a className="label-link" href="#">
+            All
+          </a>
+        </li>
+        <li className="label-item">
+          <a className="label-link" href="#">
+            Groceries
+          </a>
+        </li>
+        <li className="label-item">
+          <a className="label-link" href="#">
+            College
+          </a>
+        </li>
+        <li className="label-item">
+          <a className="label-link" href="#">
+            Payments
+          </a>
+        </li>
       </ul>
     </div>
   )
