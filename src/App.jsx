ab141c843530f90fca829d02d374b5eb8c2610da
Eduar <83920925+eduarchilon@users.noreply.github.com>
2022-06-01 13:50:19 -0300

    feat: branch dev

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
 import React, { Fragment } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/home/Home'
-// import AddTask from './pages/add-task/AddTask'
-// import EditTask from './pages/edit-task/EditTask'
+import AddTask from './pages/add-task/AddTask'
+import EditTask from './pages/edit-task/EditTask'
 
 export function App() {
   return (
     <div className='app'>
 
-      <Home />
-      {/* <BrowserRouter>
+      {/* <Home /> */}
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/addtask" element={<AddTask />} />
           <Route path="/edittask" element={<EditTask />} />
         </Routes>
-      </BrowserRouter> */}
+      </BrowserRouter>
     </div>
   )
 }
