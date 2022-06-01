162b7c486a853ca0873b1b54f54edb29edfa6cca
Javier Valdez <jv.javierval@gmail.com>
2022-05-31 22:06:34 -0300

    feat/ add components to Home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import Task from '../../components/task/Task'
+import Labels from '../../components/labels/Labels'
+import './home.css'
 
 const Home = () => {
-  return <div>home</div>
+  return (
+    <div>
+      <header className="header">
+        <h1>All Tasks</h1>
+        <input className="search-input" placeholder="Search"></input>
+      </header>
+
+      <main className="home__main-container">
+        <Labels />
+        <Task />
+      </main>
+    </div>
+  )
 }
 
 export default Home
