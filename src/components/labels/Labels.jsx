423352670668f0a2e22d12b7537c4c674e118bd7
Javier Valdez <jv.javierval@gmail.com>
2022-05-31 21:56:00 -0300

    feat/ create Labels component

---
+++ src/components/labels/Labels.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Labels.css'
+
+export default function labels() {
+  return (
+    <div className="label__container">
+      <label>All</label>
+      <label>Groceries</label>
+      <label>College</label>
+      <label>Payments</label>
+    </div>
+  )
+}
