9e8f56cbb0baff8c1ab766a79236dfc5536667a6
matias <matitonello@hotmail.com>
2022-06-02 11:42:31 -0300

    feat: Links to buttons

--- src/components/buttons/Buttons.jsx
+++ src/components/buttons/Buttons.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import './buttons.css'
+import { Link } from 'react-router-dom'
 
 const Buttons = () => {
   return (
     <div className="container__buttons">
-      <button className="btnCancel">Cancel</button>
-      <button className="btnSave">Save</button>
+      <Link to="/">
+        <button className="btnCancel">Cancel</button>
+      </Link>
+
+      <Link to="/">
+        <button className="btnSave">Save</button>
+      </Link>
     </div>
   )
 }
--- src/components/taskSection/TaskSection.jsx
+++ src/components/taskSection/TaskSection.jsx
@@ -1,14 +1,20 @@
 import TaskContainer from '../taskContainer/TaskContainer'
 import './tasksection.css'
 import '../../style.css'
+import { Link } from 'react-router-dom'
 
 export default function TaskSection() {
   return (
     <div className="task__section">
-      <h1 className='title'>All Task</h1>
-      <input type="search" placeholder='Buscar' className='input-search'/>
+      <h1 className="title">All Task</h1>
+      <input type="search" placeholder="Buscar" className="input-search" />
       <TaskContainer />
-      <button type='button' className='btn btn-plus'>+</button>
+
+      <Link to="/EditTask">
+        <button type="button" className="btn btn-plus">
+          +
+        </button>
+      </Link>
     </div>
   )
 }
