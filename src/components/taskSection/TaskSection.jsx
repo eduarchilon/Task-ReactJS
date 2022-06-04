274df8356c4f888fa2246cbca56cceba0e038d23
Javier Valdez <jv.javierval@gmail.com>
2022-06-04 17:22:00 -0300

    feat: add hr under title on mobile home

--- src/components/taskSection/TaskSection.jsx
+++ src/components/taskSection/TaskSection.jsx
@@ -7,7 +7,8 @@ export default function TaskSection() {
   return (
     <div className="task__section">
       <div>
-        <h1 className="title">All Task</h1>
+        <h1 className="title">All Tasks</h1>
+        <hr className="title__hr--mobile" />
         <input type="search" placeholder="Search" className="input-search" />
       </div>
 
@@ -15,10 +16,28 @@ export default function TaskSection() {
 
       <Link to="/AddTask">
         <button type="button" className="btn-plus">
-          <svg width="60" height="60" viewBox="0 0 60 60" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <svg
+            width="60"
+            height="60"
+            viewBox="0 0 60 60"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
             <circle cx="30" cy="30" r="30" fill="#EA5959" />
-            <path d="M30 21.6V38.4" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-            <path d="M21.6 30H38.4" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+            <path
+              d="M30 21.6V38.4"
+              stroke="white"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M21.6 30H38.4"
+              stroke="white"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
           </svg>
         </button>
       </Link>
