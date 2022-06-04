94a9e3e4312a43e49ea8babfbd873fcd7fcbfa2f
Eduar <83920925+eduarchilon@users.noreply.github.com>
2022-06-03 23:39:33 -0300

    feat: style modified

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -11,11 +11,13 @@ function Home() {
     <div className="container home">
       <div className="label__container">
         <ul className="label__list">
+          <li className="label-item"><a href="#" className="label-link">All</a></li>
           {Categories.map((Category, index) => {
             return <Labels key={index} category={Category} />
           })}
         </ul>
       </div>
+      <div className="border-home"></div>
       <TaskSection />
     </div>
   )
